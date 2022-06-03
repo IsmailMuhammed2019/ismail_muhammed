ae2eaebdfdbe58b8e4704331cae0229026dd29ab
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2022-06-03 12:34:23 +0100

    configured and migrated demo models in the models.py file

--- my_env/ismailmuhammeddjangoproj/newapp/models.py
+++ my_env/ismailmuhammeddjangoproj/newapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Schools(models.Model);
+    name = models.CharField(max_length=23)
+    address = models.CharField(max_length=23)
+
+class Country(models.model):
+    name = models.CharField(max_length=23)
